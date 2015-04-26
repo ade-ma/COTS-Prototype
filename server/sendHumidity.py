6ad8b09bcd42441766da681a68372c5f53d9a059
ihoover <polonian@gmail.com>
2015-04-26 00:25:50 -0400

    updated sendHumidity to be farm specigfic

--- server/sendHumidity.py
+++ server/sendHumidity.py
@@ -1,9 +1,9 @@
 import urllib2
 import sys
 from dropletConfig import DROPLET_IP
-
+farm = 'olin' # 'tangerinis' # 'firstlight'
 ID = sys.argv[1]
 value = sys.argv[2]
-url = "http://"+DROPLET_IP+"/humiditySense?ID=" + ID + "&value=" + value
+url = "http://"+DROPLET_IP+"/humiditySense?ID=" + ID + "&value=" + value + "&farm=" + farm
 print url
 urllib2.urlopen(url)
