8c9ca455e4e74f23a3f8916235ffa28e71bc4b51
ihoover <polonian@gmail.com>
2015-04-26 00:02:34 -0400

    made it farm specific

--- server/sendTemp.py
+++ server/sendTemp.py
@@ -1,8 +1,8 @@
 import urllib2
 import sys
 from dropletConfig import DROPLET_IP
-
+farm = 'olin' # 'tangerinis' # 'firstlight'
 ID = sys.argv[1]
 value = sys.argv[2]
-url = "http://" + DROPLET_IP + "/tempSense?ID=" + ID + "&value=" + value
+url = "http://" + DROPLET_IP + "/tempSense?ID=" + ID + "&value=" + value + "&farm=" + farm
 urllib2.urlopen(url)
